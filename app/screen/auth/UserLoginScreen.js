52a90112f4d49430d82d56639da08ffe3b719ea0
akhtar21yr <akhtar.decy@gmail.com>
2024-07-29 17:41:22 +0530

    feat:add 'Toast' msg , add forget & register button

--- app/screen/auth/UserLoginScreen.js
+++ app/screen/auth/UserLoginScreen.js
@@ -1,7 +1,9 @@
-import { View, Text, TextInput, Button } from 'react-native'
+import { View, Text, TextInput, Button, ScrollView, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import styles from '../../../style'
+import { styles, toastConfig } from '../../../style'
+import Toast from 'react-native-toast-message'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 
 
 
@@ -12,31 +14,66 @@ const UserLoginScreen = () => {
     const handleFormSumbit = () => {
         console.log('Email:', email, 'Password:', password)
         if (email && password) {
-            console.log("Login Success")
+            const formData = { email, password }
+            setEmail()
+            setPassword()
+            Toast.show({
+                text1: 'Login success',
+                type: 'done',
+                duration: 2000,
+                position: 'top',
+                topOffset: 0
+            })
         }
         else {
-            console.log("Please fill out all fields")
+            Toast.show({
+                text1: 'All fields are required',
+                type: 'warning',
+                duration: 2000,
+                position: 'top',
+                topOffset: 0
+            })
         }
     }
 
 
     return (
         <SafeAreaView>
-            <View style={{ marginHorizontal: 30 }}>
-                <View style={styles.inputWithLabel}>
-                    <Text style={styles.labelText}>Email</Text>
-                    <TextInput style={styles.input} value={email} onChangeText={setEmail} placeholder='Enter Email' keyboardType='email-address' />
+            <Toast config={toastConfig} />
+            <ScrollView keyboardShouldPersistTaps='handled'>
+
+                <View style={{ marginHorizontal: 30 }}>
+                    <View style={{ alignSelf: 'center', marginBottom: 10 }}>
+                        <MaterialIcons name='shopping-bag' color='purple' size={100} />
+                    </View>
+                    <View style={styles.inputWithLabel}>
+                        <Text style={styles.labelText}>Email</Text>
+                        <TextInput style={styles.input} value={email} onChangeText={setEmail} placeholder='Enter Email' keyboardType='email-address' />
+                    </View>
                 </View>
-            </View>
-            <View style={{ marginHorizontal: 30 }}>
-                <View style={styles.inputWithLabel}>
-                    <Text style={styles.labelText}>Password</Text>
-                    <TextInput style={styles.input} value={password} onChangeText={setPassword} placeholder='Enter Password' secureTextEntry={true} />
+                <View style={{ marginHorizontal: 30 }}>
+                    <View style={styles.inputWithLabel}>
+                        <Text style={styles.labelText}>Password</Text>
+                        <TextInput style={styles.input} value={password} onChangeText={setPassword} placeholder='Enter Password' secureTextEntry={true} />
+                    </View>
+                    <View style={{ width: 200, alignSelf: 'center', margin: 20 }}>
+                        <Button title='Login' onPress={handleFormSumbit} color='purple' />
+                    </View>
+                    <View style={{flexDirection:'row', alignSelf:'center'}}>
+                        <View style={{flex:1}}>
+                            <TouchableOpacity onPress={()=>{console.log('password reset screen')}} style={{justifyContent:'flex-start'}}>
+                                <Text style={{fontWeight:'bold',color:'blue'}}>Forget Password</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={{flex:1}}>
+                            <TouchableOpacity onPress={()=>{console.log('New Register Screen')}} style={{justifyContent:'flex-end'}}>
+                                <Text style={{fontWeight:'bold',color:'blue'}}>New User? Register</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                 </View>
-                <View style={{width:200,alignSelf:'center',margin:20}}>
-                    <Button title='Login' onPress={handleFormSumbit} color='purple' />
-                </View>
-            </View>
+
+            </ScrollView>
         </SafeAreaView>
     )
 }
--- style.js
+++ style.js
@@ -1,22 +1,51 @@
-import {StyleSheet} from 'react-native'
+import { StyleSheet, View, Text } from 'react-native';
 
 const styles = StyleSheet.create({
-    labelText:{
+    labelText: {
         fontSize: 18,
         marginBottom: 6,
-        marginLeft:7 
-      },
-    input : {
-        height:40,
-        borderWidth:1,
-        paddingHorizontal:15,
-        borderRadius:8,
-        fontSize:15
+        marginLeft: 7,
     },
-    inputWithLabel:{
-        marginBottom:10,
-        marginTop:5
-    }
-})
+    input: {
+        height: 40,
+        borderWidth: 1,
+        paddingHorizontal: 15,
+        borderRadius: 8,
+        fontSize: 15,
+    },
+    inputWithLabel: {
+        marginBottom: 10,
+        marginTop: 5,
+    },
+});
 
-export default styles;
+const toastConfig = {
+    warning: ({ text1, props }) => (
+        <View style={{ 
+            height: 30, 
+            width: '100%', 
+            backgroundColor: 'orange', 
+            justifyContent: 'center', 
+            alignItems: 'center' ,
+            paddingTop:12.5
+        }}>
+            <Text>{text1}</Text>
+            <Text>{props.uuid}</Text>
+        </View>
+    ),
+    done: ({ text1, props }) => (
+        <View style={{ 
+            height: 30, 
+            width: '100%', 
+            backgroundColor: '#1affc6', 
+            justifyContent: 'center', 
+            alignItems: 'center', 
+            paddingTop:12.5
+        }}>
+            <Text>{text1}</Text>
+            <Text>{props.uuid}</Text>
+        </View>
+    ),
+};
+
+export { styles, toastConfig };
